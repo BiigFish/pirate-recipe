bb3ad22aca6c58495fba9845f87090c0e30bfbb0
Evan <evchan29@gmail.com>
2023-09-19 16:38:32 -0400

    Revert "Revert "Revert "Revert "select category""""
    This reverts commit 3b230fef15264e5d23a0d8b427dbb2e4be062998.

--- app/recipe-form/recipe-form.tsx
+++ app/recipe-form/recipe-form.tsx
@@ -1,12 +1,23 @@
 "use client";
 
 import React, { useState } from "react";
-import { Database, Ingredient } from "../database.types";
+import { Category, Database, Ingredient } from "../database.types";
 import CompInput from "./input";
 import MultiInput from "./multi-input";
 import MultiComplexInput from "./multi-complex-input";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import CompButton from "@/assets/button";
+import CompSelect from "./select";
+
+const categoryOptions = [
+  { value: Category.BREAKFAST, label: "Breakfast" },
+  { value: Category.MEAL, label: "Meal" },
+  { value: Category.SNACK, label: "Snack" },
+  { value: Category.DESSERT, label: "Dessert" },
+  { value: Category.DRINK, label: "Drink" },
+  { value: Category.SOUP, label: "Soup" },
+  { value: Category.OTHER, label: "Other" },
+];
 
 const RecipeForm = () => {
   type Recipe = Database["public"]["Tables"]["recipes"]["Row"];
@@ -14,13 +25,14 @@ const RecipeForm = () => {
 
   const initialRecipeData: Recipe = {
     active_cook_time: null,
+    category: null,
     created_at: "",
     description: null,
     id: Math.floor(Math.random() * 1000000000),
     ingredients: [{ amount: "", ingredient: "" }],
     instructions: [""],
     name: "",
-    notes: [],
+    notes: [""],
     passive_cook_time: null,
     yield: null,
   };
@@ -69,6 +81,23 @@ const RecipeForm = () => {
         value={recipeData.yield || ""}
         onChange={(e) => handleInputChange(e.target.name, e.target.value)}
       />
+      <CompSelect
+        label="Category"
+        name="category"
+        value={recipeData.category || ""}
+        onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+        options={categoryOptions}
+      />
+      <MultiComplexInput
+        label="Ingredients"
+        values={recipeData.ingredients || []}
+        setValues={(values) => handleInputChange("ingredients", values)}
+      />
+      <MultiInput
+        label="Instructions"
+        values={recipeData.instructions || []}
+        setValues={(values) => handleInputChange("instructions", values)}
+      />
       <CompInput
         label="Active Cook Time"
         name="active_cook_time"
@@ -81,16 +110,16 @@ const RecipeForm = () => {
         value={recipeData.passive_cook_time || ""}
         onChange={(e) => handleInputChange(e.target.name, e.target.value)}
       />
-      <MultiInput
-        label="Instructions"
-        values={recipeData.instructions || []}
-        setValues={(values) => handleInputChange("instructions", values)}
+      <CompInput
+        label="Yield"
+        name="yield"
+        value={recipeData.yield || ""}
+        onChange={(e) => handleInputChange(e.target.name, e.target.value)}
       />
-
-      <MultiComplexInput
-        label="Ingredients"
-        values={recipeData.ingredients || []}
-        setValues={(values) => handleInputChange("ingredients", values)}
+      <MultiInput
+        label="Notes"
+        values={recipeData.notes || []}
+        setValues={(values) => handleInputChange("notes", values)}
       />
 
       <CompButton type="submit" fullWidth>
